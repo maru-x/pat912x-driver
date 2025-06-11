0ca40a593eaa40391b7f2f1c6e9e886bba1e68e3
maru-x <m_goshono@yahoo.co.jp>
2025-06-12 08:34:54 +0900

    pat912x_configure関数からPAT9126_AE_ENABLEのI2C書き込みをコメントアウト

--- drivers/input/input_pat912x.c
+++ drivers/input/input_pat912x.c
@@ -357,10 +357,11 @@ static int pat912x_configure(const struct device *dev)
 			return ret;
 		}
 	}
-	ret = i2c_reg_write_byte_dt(&cfg->i2c, PAT9126_AE_ENABLE, PAT9126_AE_ENABLE_VAL);
-	if (ret < 0) {
-		return ret;
-	}
+
+	// ret = i2c_reg_write_byte_dt(&cfg->i2c, PAT9126_AE_ENABLE, PAT9126_AE_ENABLE_VAL);
+	// if (ret < 0) {
+	// 	return ret;
+	// }
 
 	ret = i2c_reg_write_byte_dt(&cfg->i2c, PAT9126_NY_MIN, 0x00);
 	if (ret < 0) {
