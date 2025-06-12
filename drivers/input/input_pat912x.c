218291efd8a2b30bf95e456d1dc41c5656bac213
maru-x <m_goshono@yahoo.co.jp>
2025-06-12 09:01:29 +0900

    pat912x_configure関数でPAT9126_NY_MINのI2C書き込みを有効化

--- drivers/input/input_pat912x.c
+++ drivers/input/input_pat912x.c
@@ -363,10 +363,10 @@ static int pat912x_configure(const struct device *dev)
 	// 	return ret;
 	// }
 
-	// ret = i2c_reg_write_byte_dt(&cfg->i2c, PAT9126_NY_MIN, 0x00);
-	// if (ret < 0) {
-	// 	return ret;
-	// }
+	ret = i2c_reg_write_byte_dt(&cfg->i2c, PAT9126_NY_MIN, 0x00);
+	if (ret < 0) {
+		return ret;
+	}
 
 	return 0;
 }
